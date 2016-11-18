b6b531ea6ca98a73273343e1c8f6e5748fb7f5f8
Kishimotovn <kishimotovn@gmail.com>
2016-11-18 15:00:37 +0000

    Initial Commit

---
+++ iOSMathSwift/iOSMathSwift.h
@@ -0,0 +1,19 @@
+//
+//  iOSMathSwift.h
+//  iOSMathSwift
+//
+//  Created by Anh Phan Tran on 11/18/16.
+//  Copyright © 2016 Anh Phan Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSMathSwift.
+FOUNDATION_EXPORT double iOSMathSwiftVersionNumber;
+
+//! Project version string for iOSMathSwift.
+FOUNDATION_EXPORT const unsigned char iOSMathSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSMathSwift/PublicHeader.h>
+
+
